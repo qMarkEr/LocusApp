2059623e8c036398b514e5757f34cb5ac5f460c7
qMarkEr <korton4ik@gmail.com>
2024-04-11 04:53:19 +0300

    Icon; Contact card; basic layout

---
+++ app/src/main/java/com/marker/locus/ContactCard.kt
@@ -0,0 +1,107 @@
+package com.marker.locus
+
+import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+
+@Composable
+fun ContactCard(name : String, id : String, lst : SnapshotStateList<String>) {
+    var mode by remember {
+        mutableStateOf(false)
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AsyncImage(
+            model = "https://i.redd.it/tatkh3vtw1tc1.jpeg",
+            contentDescription = "wtf",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(80.dp)
+                .padding(15.dp)
+                .clip(CircleShape)
+        )
+        Box (modifier = Modifier
+            .fillMaxHeight()
+            .padding(horizontal = 0.dp, vertical = 20.dp)) {
+            Text(
+                text = name,
+                modifier = Modifier.align(Alignment.TopStart),
+                textAlign = TextAlign.Center,
+
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = FontFamily.Default
+            )
+            Text(text = "Username: $id",
+                modifier = Modifier
+                    .align(Alignment.BottomStart),
+                textAlign = TextAlign.Center,
+
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Light,
+                fontFamily = FontFamily.Default
+            )
+        }
+
+        Spacer(
+            Modifier
+                .weight(2f)
+                .fillMaxHeight()
+        )
+        IconButton(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .size(70.dp)
+                .padding(15.dp),
+            onClick = {
+                mode = !mode
+                if (mode)
+                    lst += name
+                else
+                    lst -= name
+            }
+        ) {
+            Icon(
+                painter = painterResource(id =
+                if (mode)
+                    R.drawable.cancel_locating
+                else
+                    R.drawable.locate),
+                contentDescription = "main action"
+            )
+        }
+    }
+}
--- app/src/main/java/com/marker/locus/MainActivity.kt
+++ app/src/main/java/com/marker/locus/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.marker.locus
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.BottomSheetScaffoldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.marker.locus.ui.theme.LocusTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val lst = mutableStateListOf("");
         setContent {
             LocusTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +47,44 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                     BottomSheetScaffold (
+                         sheetContent = {
+                             Column (modifier = Modifier.fillMaxHeight()) {
+                                 Text(
+                                     text = "Contacts",
+                                     fontFamily = FontFamily.Default,
+                                     fontWeight = FontWeight.Bold,
+                                     modifier = Modifier
+                                         .align(Alignment.CenterHorizontally)
+                                         .padding(20.dp),
+                                     textAlign = TextAlign.Center,
+                                     fontSize = 30.sp
+                                 )
+                                 TextButton(onClick = { /*TODO*/ }, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                                     Icon(
+                                         imageVector = Icons.Default.Add,
+                                         contentDescription = "add",
+                                         modifier = Modifier.size(18.dp)
+                                     )
+                                     Spacer(modifier = Modifier.width(8.dp))
+                                     Text(text = "Add new")
+                                 }
+                                 ContactCard("Scoof Borov", id = "3et21f", lst)
+                                 ContactCard("Alt borovitski", id = "4hg4q23hgb", lst)
+                                 ContactCard("Alexiy Chuvashskiy", id = "4hb3qh34", lst)
+                             }
+                         },
+                         content =  {
+                            LazyColumn {
+                              items(lst.size) {
+                                  Text(text = lst[it])
+                              }
+                            }
+                         },
+                         sheetPeekHeight = BottomSheetScaffoldDefaults.SheetPeekHeight
+                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LocusTheme {
-        Greeting("SUs")
     }
 }
